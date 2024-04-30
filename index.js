6c5737a1c5fdac763528a1f0ee9bbd69c0744471
Nduke Sam <diamondsam33@gmail.com>
2024-04-30 22:49:54 +0100

    env variables

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 app.listen(3000, ()=> {
+  console.log(api);
   console.log('Server running http://localhost:3000');
 })
 
