39d0c00232fd52f4ad1dd7cd1befe96cce2963cd
Nduke Sam <diamondsam33@gmail.com>
2024-05-02 14:45:19 +0100

    database working

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-require('dotenv/config');
+require("dotenv/config");
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
 const api = process.env.API_URL;
 
-app.listen(3000, ()=> {
-  console.log(api);
-  console.log('Server running http://localhost:3000');
-})
+mongoose
+  .connect(process.env.COLLECTION_STRING)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(api);
+      console.log("Server running http://localhost:3000");
+    });
 
-app.get('/', (req, res)=> {
-  res.send('Hello API!')
-})
-const func = () => {
-  return "hello";
-};
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+    // throw new Error('Database connection unsuccessful')
+  });
 
-func()
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "high-top sneakers",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+});
+
+// const func = () => {
+//   return "hello";
+// };
+
+// func()
 
 // function String(letters) {
 //   this.letters = letters;
